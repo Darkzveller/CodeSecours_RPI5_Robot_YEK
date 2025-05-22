0b4028d2d4f2e78afe87163784a989e16024e652
youssef <youssef.el-kattoufi@universite-paris-saclay.fr>
2025-05-22 18:58:07 +0200

    Code lidar tester sur windows

--- lib/LIDAR/LIDAR.py
+++ lib/LIDAR/LIDAR.py
@@ -2,32 +2,47 @@
 from rplidar import RPLidar
 import time
 lidar = None
-
-# Fonction d'initialisation du LiDAR
-def setup_lidar():
-    global lidar  # on précise qu'on utilise la variable globale
-    lidar = RPLidar("COM6", baudrate=256000)
-    print(lidar.get_info())
-    lidar.start_motor()
-    time.sleep(1)
-    print('Initialisation du lidar')
+scan_iterator = None
 
 
 # Fonction de lecture des données du LiDAR
-def read_lidar():
-    global lidar  # on précise qu'on utilise la variable globale
+# def read_lidar():
+#     global lidar  # on précise qu'on utilise la variable globale
 
-    for scan in lidar.iter_scans():
-        for (_, angle, distance) in scan:
-            print(f'Angle: {angle:.2f}°, Distance: {distance:.2f} mm')
-            if 0 < distance < 300:
-                print(f'Stop! Object detected at {distance:.2f} mm, angle {angle:.2f}°')
+#     for scan in lidar.iter_scans():
+#         for (_, angle, distance) in scan:
+#             print(f'Angle: {angle:.2f}°, Distance: {distance:.2f} mm')
+#             if 0 < distance < 300:
+#                 print(f'Stop! Object detected at {distance:.2f} mm, angle {angle:.2f}°')
+#                 print('\n')
+def read_lidar():
+    global lidar
+    global scan_iterator
+    scan = next(scan_iterator)  # récupère un scan depuis l'itérateur existant, non bloquant ici
+    for (_, angle, distance) in scan:
+        # print(f'Angle: {angle:.2f}°, Distance: {distance:.2f} mm')
+        if 0 < distance < 300:
+            print(f'Stop! Object detected at {distance:.2f} mm, angle {angle:.2f}°')
 
 # Fonction pour arrêter le LiDAR proprement
-def stop_lidar_fonctionnement():
-    global lidar  # on précise qu'on utilise la variable globale
+def stop_lidar():
+    global lidar,scan_iterator  # on précise qu'on utilise la variable globale
     lidar.stop_motor()
     lidar.stop()
     time.sleep(1)
     lidar.disconnect()
+    print('stop lidar')
+    print('\n')
+
+# Fonction d'initialisation du LiDAR
+def setup_lidar():
+    global lidar,scan_iterator  # on précise qu'on utilise la variable globale
+    lidar = RPLidar("COM18", baudrate=256000)
+    print(lidar.get_info())
+    print('lidar',lidar)
+    lidar.start_motor()
+    scan_iterator = lidar.iter_scans()  # une seule fois ici
+    time.sleep(5)
+    print('Initialisation du lidar')
+    print('\n')
 
--- main.py
+++ main.py
@@ -1,20 +1,27 @@
-# import LIDAR
-# import CAN
+import time
+import keyboard
 from lib.CAN import CAN
 from lib.LIDAR import LIDAR
 
-# Initialisation des fonctions des variables
-# LIDAR.setup_lidar()
+# Initialisation des fonctions des 
+LIDAR.setup_lidar()
 # CAN.setup_can()
 test = False
 valeur =300
 poids_fort,poids_faible = CAN.separation_en_octets(valeur)
-# poids_faible = valeur & 0xFF        # Masque sur les 8 bits de droite
+poids_faible = valeur & 0xFF        # Masque sur les 8 bits de droite
 
 print(f"Poids fort : {poids_fort} (0x{poids_fort:02X})")
 print(f"Poids faible : {poids_faible} (0x{poids_faible:02X})")
 i = 0 
 
-# while (True) : 
-    # i=i+1
-    # print("i",i)
+
+while True:
+    i += 1
+    LIDAR.read_lidar()
+    print("i = ", i)
+    if keyboard.is_pressed('Q'):
+        LIDAR.stop_lidar()
+        print("Touche 'S' détectée. Arrêt du programme.")
+        break
+    time.sleep(0.025)
